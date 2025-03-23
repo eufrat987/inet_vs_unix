5877adf1029d04ccb0ea7ab28b97f52b9e2ece4d
eufrat <lukzyw@gmail.com>
2025-03-23 13:33:42 +0100

    Unix Server - 2. Remove socket file at the end

--- unix/server/server.c
+++ unix/server/server.c
@@ -5,28 +5,36 @@
 #include <sys/un.h>
 
 #define PROJECT_NAME "server"
-
-void handle_error(int status, char* msg) {
-    if (status == -1) {
-    	perror(msg);
-	exit(EXIT_FAILURE);
-    }  
-}
+#define SOCKET_PATH "./unix_socket"
 
 int main(int argc, char **argv) {
     printf("This is project %s.\n", PROJECT_NAME);
     
     int connection_socket = socket(AF_UNIX, SOCK_STREAM, 0);
-    handle_error(connection_socket, "socket");
+    printf("Fd %d \n", connection_socket);
+    if (connection_socket == -1) {
+    	perror("socket");
+	exit(EXIT_FAILURE);
+    } 
 
     struct sockaddr_un my_addr;
     memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sun_family = AF_UNIX;
+    strncpy(my_addr.sun_path, SOCKET_PATH, sizeof(my_addr.sun_path));
+
     int ret = bind(connection_socket, (struct sockaddr *) &my_addr, sizeof(my_addr)); 
-    handle_error(ret, "bind");
+    if (ret == -1) {
+    	perror("bind");
+	exit(EXIT_FAILURE);
+	close(connection_socket);
+    } 
+
     //int fd2 = connect(fd, 
 
+    //while(1){}
 
+    
+    unlink(SOCKET_PATH);
     close(connection_socket);
-
     return 0;
 }
