2287635d1d95015896746d18043464cc433e5f89
eufrat <lukzyw@gmail.com>
2025-03-23 12:09:43 +0100

    Unix Server - 1. Connect Socket

--- unix/server/server.c
+++ unix/server/server.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 #define PROJECT_NAME "server"
 
+void handle_error(int status, char* msg) {
+    if (status == -1) {
+    	perror(msg);
+	exit(EXIT_FAILURE);
+    }  
+}
+
 int main(int argc, char **argv) {
-    if(argc != 1) {
-        printf("%s takes no arguments.\n", argv[0]);
-        return 1;
-    }
     printf("This is project %s.\n", PROJECT_NAME);
+    
+    int connection_socket = socket(AF_UNIX, SOCK_STREAM, 0);
+    handle_error(connection_socket, "socket");
+
+    struct sockaddr_un my_addr;
+    memset(&my_addr, 0, sizeof(my_addr));
+    int ret = bind(connection_socket, (struct sockaddr *) &my_addr, sizeof(my_addr)); 
+    handle_error(ret, "bind");
+    //int fd2 = connect(fd, 
+
+
+    close(connection_socket);
+
     return 0;
 }
