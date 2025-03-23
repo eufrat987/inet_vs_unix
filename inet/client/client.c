ede931394888f3f9f026edcca55780e297f04497
eufrat <lukzyw@gmail.com>
2025-03-23 18:58:08 +0100

    Inet - Client & Server

--- inet/client/client.c
+++ inet/client/client.c
@@ -1,12 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
 
 #define PROJECT_NAME "client"
+#define TO_SEND 1024LL * 1024 * 1024 // 1GB
+#define BUFFER_SIZE 20
+#define LISTEN_PORT 8080
 
 int main(int argc, char **argv) {
-    if(argc != 1) {
-        printf("%s takes no arguments.\n", argv[0]);
-        return 1;
-    }
     printf("This is project %s.\n", PROJECT_NAME);
+
+    int connection_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (connection_socket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_port = htons(LISTEN_PORT);
+    addr.sin_family = AF_INET;
+
+    int ret = connect(connection_socket, (const struct sockaddr *) &addr, sizeof(addr));
+    if (ret == -1) {
+        fprintf(stderr, "The server is down.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    long long int to_send = TO_SEND;
+    char buffer[BUFFER_SIZE];
+    memset(&buffer, 1, sizeof(buffer));
+    while(to_send > 0) {
+        size_t w = write(connection_socket, buffer, sizeof(buffer));
+        if (w == -1) {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+        to_send -= w;
+    }
+
+    printf("Sended %lld bytes\n", TO_SEND - to_send);
+
     return 0;
 }
--- inet/server/server.c
+++ inet/server/server.c
@@ -1,12 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
 
 #define PROJECT_NAME "server"
+#define LISTEN_PORT 8080
+#define LISTEN_BACKLOG 1
+#define BUFFER_SIZE 20
+#define TO_RECEIVE 1024LL * 1024 * 1024 // 1GB
 
 int main(int argc, char **argv) {
-    if(argc != 1) {
-        printf("%s takes no arguments.\n", argv[0]);
-        return 1;
-    }
     printf("This is project %s.\n", PROJECT_NAME);
+
+    int connection_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (connection_socket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_port = htons(LISTEN_PORT);
+    addr.sin_family = AF_INET;
+
+    int ret = bind(connection_socket, (struct sockaddr *) &addr, sizeof(addr));            
+    if (ret == -1) {
+        perror("bind");
+    	close(connection_socket);
+	exit(EXIT_FAILURE);
+    }
+    
+    ret = listen(connection_socket, LISTEN_BACKLOG);
+    if (ret == -1) {
+        perror("listen");
+    	close(connection_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in peer_addr;
+    socklen_t peer_addr_size;
+    int client_fd = accept(connection_socket, (struct sockaddr *) &peer_addr, &peer_addr_size);
+    if (client_fd == -1) {
+        perror("accept");
+    	close(connection_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    long long int to_receive = TO_RECEIVE;
+    char buffer[BUFFER_SIZE];
+
+    while (to_receive > 0) {
+        ssize_t r = read(client_fd, buffer, sizeof(buffer));
+        to_receive -= r;
+    }
+
+    printf("Received %lld bytes\n", TO_RECEIVE - to_receive);
+
+    close(connection_socket);
     return 0;
 }
