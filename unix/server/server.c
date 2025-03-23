fabb2b5d80695aa18f326a48c6a5d06a9fd0169d
eufrat <lukzyw@gmail.com>
2025-03-23 15:33:18 +0100

    Unix Server - X. Handle clean up

--- unix/server/server.c
+++ unix/server/server.c
@@ -3,15 +3,37 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <signal.h>
 
 #define PROJECT_NAME "server"
 #define SOCKET_PATH "./unix_socket"
+#define LISTEN_BACKLOG 1
+#define BUFFER_SIZE 20
+
+int connection_socket = -1;
+
+void cleanup() {
+    printf("Clean up\n");
+    close(connection_socket);
+    unlink(SOCKET_PATH);
+}
+
+void add_signals_handler() {
+    if (signal(SIGINT, cleanup) == SIG_ERR) {
+	perror("signal");
+	exit(EXIT_FAILURE);
+    } 
+    if (signal(SIGTERM, cleanup) == SIG_ERR) {
+	perror("signal");
+	exit(EXIT_FAILURE);
+    } 
+}
 
 int main(int argc, char **argv) {
     printf("This is project %s.\n", PROJECT_NAME);
+    add_signals_handler();
     
-    int connection_socket = socket(AF_UNIX, SOCK_STREAM, 0);
-    printf("Fd %d \n", connection_socket);
+    connection_socket = socket(AF_UNIX, SOCK_STREAM, 0);
     if (connection_socket == -1) {
     	perror("socket");
 	exit(EXIT_FAILURE);
@@ -25,16 +47,35 @@ int main(int argc, char **argv) {
     int ret = bind(connection_socket, (struct sockaddr *) &my_addr, sizeof(my_addr)); 
     if (ret == -1) {
     	perror("bind");
+	cleanup();
 	exit(EXIT_FAILURE);
-	close(connection_socket);
     } 
 
-    //int fd2 = connect(fd, 
+    ret = listen(connection_socket, LISTEN_BACKLOG); 
+    if (ret == -1) {
+    	perror("listen");
+	cleanup();
+	exit(EXIT_FAILURE);
+    }
 
-    //while(1){}
+    struct sockaddr_un peer_addr;
+    socklen_t peer_addr_size;
+    int client_fd = accept(connection_socket, (struct sockaddr *) &peer_addr, &peer_addr_size); 
+    if (client_fd == -1) {
+    	perror("accept");
+	cleanup();
+	exit(EXIT_FAILURE);
+    }
+   
+    int to_receive = 5;
+    char buffer[BUFFER_SIZE];
 
-    
-    unlink(SOCKET_PATH);
-    close(connection_socket);
+    while (to_receive > 0) {
+	ssize_t r = read(client_fd, buffer, sizeof(buffer)); 
+	to_receive -= r;
+    }
+
+
+    cleanup();
     return 0;
 }
